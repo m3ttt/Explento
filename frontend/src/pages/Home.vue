d47a3f1fa83773e85ab7d550182f7578f4882eef
Matteo Quagliani <matteoquagliani3@gmail.com>
2025-11-25 14:09:00 +0100

    add: primi file del progetto

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Explento!</h1>
+    <p>Home page</p>
+  </div>
+</template>
+
+<script setup>
+// Logica interna
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
