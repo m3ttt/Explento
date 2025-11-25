0f2a7a718991a6b78b0990f66251129c89d9c58e
m3ttt <>
2025-11-25 14:09:00 +0100

    add: primi file del progetto

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/map">Map</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background: #eee;
+  padding: 1rem;
+}
+</style>
