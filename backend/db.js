e29a71c801d3b4419e21095aebe02f87ccb8b881
m3ttt <>
2025-11-26 13:21:00 +0100

    fix: Prettier file globale

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export async function connectDatabase() {
-  try {
-    let client = await mongoose.connect(process.env.MONGO_DB_URL);
-    console.log("Connesso a MongoDB");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    try {
+        let client = await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("Connesso a MongoDB");
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
