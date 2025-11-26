e634041479082dc244cae82171cd37a01ca0bd41
Mat12143 <matteo.peretto@studenti.unitn.it>
2025-11-26 21:32:04 +0100

    add: /users endpoint + typescript conversion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,9 +9,9 @@ async function startServer() {
     try {
         await connectDatabase(); // connessione a MongoDB
         const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () =>
-            console.log(`Server avviato sulla porta ${PORT}`),
-        );
+        app.listen(PORT, () => {
+            console.log(`Server avviato sulla porta ${PORT}`);
+        });
     } catch (err) {
         console.error("Errore avvio server:", err);
     }
