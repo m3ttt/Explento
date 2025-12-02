c74dcbb639f410a90d43a176c74fd322f963b232
m3ttt <>
2025-12-02 16:51:08 +0100

    feat: aggiunta logica per il completamento missione refactor: ristrutturato meController
    - handler triggerVisitPlace spezzato in sottofunzioni più piccole
    - aggiunti commenti esplicativi
    - migliorata la leggibilità complessiva

--- backend/src/models/Place.ts
+++ backend/src/models/Place.ts
@@ -1,16 +1,20 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
 const PlaceSchema = new Schema({
-    name: String,
+    name: {type: String, required: true},
     description: String,
-    categories: [String],
+    categories: {type: [String], required: true},
     location: {
-        lat: Number,
-        lon: Number,
+        lat: { type: Number, required: true },
+        lon: { type: Number, required: true },
+        _id: false, // evita che Mongoose crei un _id per location
     },
-    images: [String],
-    isFree: Boolean,
+    images: [String], // opzionale
+    isFree: { type: Boolean, required: true },
 });
 
+// Tipo TypeScript del documento
 export type PlaceType = InferSchemaType<typeof PlaceSchema>;
+
+// Modello Mongoose
 export const Place = model("Place", PlaceSchema);
