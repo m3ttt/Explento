2a30b4750b1c92da5a65974ad80555f5238c6b63
m3ttt <>
2025-12-03 08:56:48 +0100

    chore: aggiunto lo schema DB PlaceEditRequest

---
+++ backend/src/models/PlaceEditRequest.ts
@@ -0,0 +1,39 @@
+import { Schema, model, InferSchemaType } from "mongoose";
+
+const PlaceEditRequestSchema = new Schema(
+    {
+        placeId: {
+            type: Schema.Types.ObjectId,
+            ref: "Place",
+            required: true,
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        // Contiene solo i campi che l'utente ha modificato rispetto all'originale
+        proposedChanges: {
+            type: Object,
+            required: true,
+        },
+
+        status: {
+            type: String,
+            enum: ["pending", "approved", "rejected"],
+            default: "pending",
+        },
+
+        operatorId: {
+            type: Schema.Types.ObjectId,
+            ref: "Operator",
+        },
+
+        operatorComment: String,
+    },
+    { timestamps: true }
+);
+
+export type PlaceEditRequestType = InferSchemaType<typeof PlaceEditRequestSchema>;
+export const PlaceEditRequest = model("PlaceEditRequest", PlaceEditRequestSchema);
