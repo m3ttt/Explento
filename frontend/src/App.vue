b13997a9983de9bbe946f05f483a75eb08adc011
Mat12143 <matteo.peretto@studenti.unitn.it>
2025-12-10 13:58:20 +0100

    add: mappa + drawer ricerca luoghi

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,4 @@
 <template>
-    <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-    <nav>
-        <RouterLink to="/">Go to Home</RouterLink>
-        <RouterLink to="/admin">Go to Admin</RouterLink>
-    </nav>
     <main>
         <RouterView />
     </main>
@@ -14,12 +9,3 @@ export default {
     name: "App",
 };
 </script>
-
-<style>
-nav {
-    margin-bottom: 1em;
-}
-nav a {
-    margin-right: 1em;
-}
-</style>
