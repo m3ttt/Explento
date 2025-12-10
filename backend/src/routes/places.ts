b15078bdca6562b245797ed230a631c83697d779
m3ttt <>
2025-12-10 13:38:50 +0100

    chore: sistemata API updatePlace

--- backend/src/routes/places.ts
+++ backend/src/routes/places.ts
@@ -3,13 +3,13 @@ import { authenticate } from "./auth.js";
 import {
     createAddPlaceRequest,
     getPlaces,
-    updatePlace,
+    createUpdatePlaceRequest,
 } from "../controllers/placeController.js";
 
 const router = Router();
 
 router.get("/", getPlaces); // Lista tutti i luoghi
 router.post("/request", authenticate, createAddPlaceRequest); // Aggiungi un nuovo luogo
-router.put("/:id", updatePlace); // Modifica un luogo esistente
+router.put("/:id", createUpdatePlaceRequest); // Modifica un luogo esistente
 
 export default router;
