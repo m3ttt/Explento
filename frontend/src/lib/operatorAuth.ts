ba66d8609bd07ca2fa3c6631c76c7f4a0dabc54a
Mat12143 <skymatti12@gmail.com>
2026-02-01 14:46:31 +0100

    add: autenticazione operatore + fix: bug head html

---
+++ frontend/src/lib/operatorAuth.ts
@@ -0,0 +1,70 @@
+import { API_ENDPOINT } from "./config";
+import { ref, type Ref } from "vue";
+import { OperatorSchema, type Operator } from "./types/operator";
+
+// Variabile che rappresenta l'utente
+let operator = ref<Operator | null>(null);
+
+export async function checkAuthOp(): Promise<Ref<Operator | null>> {
+    operator.value = null;
+
+    const token = localStorage.getItem("token");
+
+    // No JWT Token, no operator
+    if (!token) return operator;
+
+    // Provo a fare /operator/me con il token
+    const resp = await fetch(`${API_ENDPOINT}/operator/me`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    if (!resp.ok) {
+        return operator;
+    }
+    const data = await resp.json();
+
+    const convertionResp = await OperatorSchema.safeParseAsync(data);
+
+    if (!convertionResp.success) {
+        console.error(convertionResp.error);
+        return operator;
+    }
+
+    operator.value = convertionResp.data;
+    return operator;
+}
+
+export async function loginOp(
+    email: string,
+    password: string,
+): Promise<{ error: boolean }> {
+    const resp = await fetch(`${API_ENDPOINT}/operator/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    });
+
+    if (!resp.ok) {
+        return { error: true };
+    }
+
+    const data = await resp.json();
+    const token = data["token"];
+
+    localStorage.setItem("token", token);
+
+    return { error: false };
+}
+
+export async function logoutOp() {
+    // Imposto user a null per farlo sloggare
+    operator.value = null;
+    localStorage.setItem("token", "");
+}
---
+++ frontend/src/views/OperatorLoginView.vue
@@ -0,0 +1,112 @@
+<script lang="ts" setup>
+import { Button } from "../components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { loginOp } from "@/lib/operatorAuth";
+
+const router = useRouter();
+
+// Dati del form di login
+const formData = ref({
+    username: "",
+    password: "",
+});
+
+const loading = ref(false);
+const error = ref("");
+
+async function triggerLogin() {
+    loading.value = true;
+    error.value = "";
+
+    const res = await loginOp(formData.value.username, formData.value.password);
+
+    if (res.error) {
+        error.value = "Login fallito. Username o password errati";
+        loading.value = false;
+        return;
+    }
+
+    loading.value = false;
+    router.push("/operator");
+    return;
+}
+</script>
+
+<template>
+    <div
+        class="w-full h-screen flex items-center justify-center bg-background px-4"
+    >
+        <Card class="w-full max-w-sm shadow-lg border-0">
+            <CardHeader class="space-y-1 text-center">
+                <CardTitle class="text-2xl font-bold">Accesso</CardTitle>
+                <CardDescription>
+                    Inserisci le tue credenziali per entrare nel Pannello
+                    Operatore
+                </CardDescription>
+            </CardHeader>
+
+            <form @submit.prevent="triggerLogin">
+                <CardContent class="grid gap-4">
+                    <div
+                        v-if="error"
+                        class="p-3 text-sm text-background font-semibold bg-destructive rounded-md border border-destructive text-center"
+                    >
+                        {{ error }}
+                    </div>
+
+                    <div class="grid gap-2">
+                        <Label for="username">Username</Label>
+                        <Input
+                            id="username"
+                            type="text"
+                            v-model="formData.username"
+                            placeholder="operatore@gmail.com"
+                            required
+                            class="bg-background"
+                        />
+                    </div>
+
+                    <div class="grid gap-2">
+                        <div class="flex items-center justify-between">
+                            <Label for="password">Password</Label>
+                        </div>
+                        <Input
+                            id="password"
+                            type="password"
+                            placeholder="operatore"
+                            v-model="formData.password"
+                            required
+                            class="bg-background"
+                        />
+                    </div>
+                </CardContent>
+
+                <CardFooter class="flex flex-col gap-4 pt-6">
+                    <Button
+                        class="w-full font-semibold"
+                        type="submit"
+                        :disabled="loading"
+                    >
+                        <span
+                            v-if="loading"
+                            class="mr-2 h-4 w-4 animate-spin rounded-full border-2 border-current border-t-transparent"
+                        ></span>
+                        {{ loading ? "Accesso in corso..." : "Accedi" }}
+                    </Button>
+                </CardFooter>
+            </form>
+        </Card>
+    </div>
+</template>
