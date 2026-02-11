ff4e0a04e153722b9f0d54601b21d3733cb56537
m3ttt <>
2026-02-11 17:18:26 +0100

    feat: aggiunti endpoints API per aggiungere/rimuovere missioni all'utente

--- backend/src/routes/mission.ts
+++ backend/src/routes/mission.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticate } from "./auth.js";
-import { getAllMissions, getAvailableMissions } from "../controllers/missionController.js";
+import { getAllMissions, activateMission, getAvailableMissions, removeMission} from "../controllers/missionController.js";
 
 const router = express.Router();
 
@@ -11,5 +11,11 @@ router.get("", getAllMissions);
 
 // GET /missions/available, missioni ancora disponibili per l'utente
 router.get("/available", getAvailableMissions);
+
+// POST /missions/activate, attiva una missione per l'utente
+router.post("/activate", activateMission);
+
+// DELETE /missions/:missionId, utente rinuncia a una missione
+router.delete("/:missionId", removeMission);
 
 export default router;
