8cca0d1fdbbe544c3c7f4357fa2e3f088d8507a9
m3ttt <>
2026-02-13 23:50:55 +0100

    fix: aggiornati i percorsi Jest per testare solo i file .ts in /testing e ignorare dist/

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,4 +3,6 @@ export default {
   testEnvironment: "node",
   verbose: true,
   setupFiles: ["dotenv/config"], // variabili d'ambiente
+  testMatch: ["<rootDir>/testing/**/*.test.ts"], // assicura di testare solo i .ts per i tests e non altri
+  testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
