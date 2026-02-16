0bf0f88e0e0393b442fcc44a6d1d796ce04c7d1f
Matteo Quagliani <matteoquagliani3@gmail.com>
2026-02-16 17:57:56 +0100

    chore: aggiunto jest.setup per ignorare warnings ed errori stampati dalle API durante il testing

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,6 +3,7 @@ export default {
   testEnvironment: "node",
   verbose: true,
   setupFiles: ["dotenv/config"], // variabili d'ambiente
+  setupFilesAfterEnv: ["<rootDir>/testing/jest.setup.ts"],
   testMatch: ["<rootDir>/testing/**/*.test.ts"], // assicura di testare solo i .ts per i tests e non altri
   testPathIgnorePatterns: ["<rootDir>/dist/"],
 };
