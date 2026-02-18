668dfa20ec97e1852294fbcbd4a77ea86ccfedb8
Mat12143 <matteo.peretto@studenti.unitn.it>
2026-02-18 14:03:01 +0100

    feat: aggiunta schermata errore api fix: bordo pulsanti avanti-indietro nei luoghi

--- frontend/src/lib/router.ts
+++ frontend/src/lib/router.ts
@@ -11,103 +11,108 @@ import OperatorRequests from "../views/OperatorRequests.vue";
 import OperatorHomeView from "../views/OperatorHomeView.vue";
 import OperatorLayout from "../components/OperatorLayout.vue";
 import MissionStats from "../views/MissionStats.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const routes = [
-    {
-        path: "/",
-        component: HomeView,
-        meta: { userAuth: true },
-        props: (route: RouteLocation) => ({
-            currentUser: route.meta.currentUser,
-        }),
-    },
-    // Gruppo operatore con layout
-    {
-        path: "/operator",
-        component: OperatorLayout,
-        meta: { operatorAuth: true },
-        props: (route: RouteLocation) => ({
-            currentOperator: route.meta.currentOperator,
-        }),
-        children: [
-            {
-                path: "", // Corrisponde a /operator
-                component: OperatorHomeView,
-                meta: { hideNavbar: true },
-            },
-            {
-                path: "heatmap", // Corrisponde a /operator/heatmap
-                component: HeatmapMissions,
-            },
-            {
-                path: "dashboard",
-                component: MissionStats,
-            },
-            {
-                path: "requests",
-                component: OperatorRequests,
-            },
-        ]
-    },
-    {
-        path: "/operator/login",
-        component: OperatorLoginView,
-    },
-    {
-        path: "/survey",
-        component: SurveyView,
-        meta: { userAuth: true },
-        props: (route: RouteLocation) => ({
-            currentUser: route.meta.currentUser,
-        }),
-    },
-    { path: "/login", component: LoginView },
-    { path: "/register", component: RegisterView },
+  {
+    path: "/",
+    component: HomeView,
+    meta: { userAuth: true },
+    props: (route: RouteLocation) => ({
+      currentUser: route.meta.currentUser,
+    }),
+  },
+  // Gruppo operatore con layout
+  {
+    path: "/operator",
+    component: OperatorLayout,
+    meta: { operatorAuth: true },
+    props: (route: RouteLocation) => ({
+      currentOperator: route.meta.currentOperator,
+    }),
+    children: [
+      {
+        path: "", // Corrisponde a /operator
+        component: OperatorHomeView,
+        meta: { hideNavbar: true },
+      },
+      {
+        path: "heatmap", // Corrisponde a /operator/heatmap
+        component: HeatmapMissions,
+      },
+      {
+        path: "dashboard",
+        component: MissionStats,
+      },
+      {
+        path: "requests",
+        component: OperatorRequests,
+      },
+    ],
+  },
+  {
+    path: "/operator/login",
+    component: OperatorLoginView,
+  },
+  {
+    path: "/survey",
+    component: SurveyView,
+    meta: { userAuth: true },
+    props: (route: RouteLocation) => ({
+      currentUser: route.meta.currentUser,
+    }),
+  },
+  { path: "/login", component: LoginView },
+  { path: "/register", component: RegisterView },
+  {
+    path: "/error",
+    component: ErrorView,
+  },
 ];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 router.beforeEach(async (to, _, next) => {
-    // Se route operatore faccio auth operatore
-    if (to.path.startsWith("/operator")) {
-        const operator = await checkAuthOp();
-        if (to.path == "/operator/login" && operator.value) {
-            return next("/operator");
-        }
+  // Non fare nessun controllo se pagina di errore
+  if (to.path.startsWith("/error")) return next();
 
-        if (to.meta.operatorAuth) {
-            if (!operator.value) {
-                return next("/operator/login");
-            }
-            to.meta.currentOperator = operator;
-        }
-        return next();
+  // Se route operatore faccio auth operatore
+  if (to.path.startsWith("/operator")) {
+    const operator = await checkAuthOp();
+    if (to.path == "/operator/login" && operator.value) {
+      return next("/operator");
     }
 
-    // Route utente
-    const user = await checkAuth();
-
-    // Redirect utente se prova a fare /login ma autenticato
-    if ((to.path == "/login" || to.path == "/register") && user.value) {
-        return next("/");
+    if (to.meta.operatorAuth) {
+      if (!operator.value) {
+        return next("/operator/login");
+      }
+      to.meta.currentOperator = operator;
     }
-
-    if (to.meta.userAuth) {
-        if (!user.value) {
-            return next(`/login`);
-        }
-
-        to.meta.currentUser = user;
-
-        if (
-            user.value.preferences?.categories.length == 0 &&
-            to.path != "/survey"
-        )
-            return next("/survey");
-    }
-
     return next();
+  }
+
+  // Route utente
+  const user = await checkAuth();
+
+  // Redirect utente se prova a fare /login ma autenticato
+  if ((to.path == "/login" || to.path == "/register") && user.value) {
+    return next("/");
+  }
+
+  if (to.meta.userAuth) {
+    if (!user.value) {
+      return next(`/login`);
+    }
+
+    to.meta.currentUser = user;
+
+    if (user.value.preferences?.categories.length == 0 && to.path != "/survey")
+      return next("/survey");
+  }
+
+  return next();
 });
