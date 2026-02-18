c75d4a6a53d54f9cf8c1024cc4935da7f410866a
Mat12143 <matteo.peretto@studenti.unitn.it>
2026-02-18 11:11:52 +0100

    fix: tasto modifica fisso

--- frontend/src/components/PlaceCard.vue
+++ frontend/src/components/PlaceCard.vue
@@ -74,36 +74,38 @@ const isToDiscover = computed(() => {
   <div
     class="relative flex flex-col w-48 h-56 gap-4 p-3 rounded-2xl bg-background snap-center shrink-0 overflow-hidden group cursor-pointer transition-all shadow-sm"
   >
-    <div class="absolute top-2 right-2 z-20 flex gap-1">
-      <Button
-        variant="secondary"
-        size="icon"
-        class="w-7 h-7 rounded-full opacity-0 group-hover:opacity-100 transition-opacity"
-        @click.stop="handleEdit"
-      >
-        <Pencil class="w-3.5 h-3.5" />
-      </Button>
-
-      <div
-        v-if="!place.isFree"
-        class="bg-amber-100 text-amber-700 p-1.5 rounded-full shadow-sm"
-      >
-        <Ticket class="w-3 h-3" />
-      </div>
-    </div>
-
     <div class="relative z-10 flex flex-col justify-between h-full">
-      <div
-        class="flex justify-start items-start gap-1 flex-wrap content-start pr-8"
-      >
-        <Badge
-          v-for="category in parsedCategories"
-          :key="category"
-          variant="secondary"
-          class="text-[10px] px-1.5 py-0 h-5"
+      <div class="flex flex-row gap-2">
+        <div
+          class="flex justify-start items-start gap-1 flex-wrap content-start pr-8"
         >
-          {{ category }}
-        </Badge>
+          <Badge
+            v-for="category in parsedCategories"
+            :key="category"
+            variant="secondary"
+            class="text-[10px] px-1.5 py-0 h-5"
+          >
+            {{ category }}
+          </Badge>
+        </div>
+
+        <div class="flex justify-between items-start gap-2">
+          <Button
+            variant="secondary"
+            size="icon"
+            class="w-7 h-7 rounded-full transition-opacity"
+            @click.stop="handleEdit"
+          >
+            <Pencil class="w-3.5 h-3.5" />
+          </Button>
+
+          <div
+            v-if="!place.isFree"
+            class="bg-amber-100 text-amber-700 p-1.5 rounded-full"
+          >
+            <Ticket class="w-4 h-4" />
+          </div>
+        </div>
       </div>
 
       <div>
